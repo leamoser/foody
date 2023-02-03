ab07d02d43f713b086d28d35ef0fb53f3e763557
Lea Moser <lea@kampakollektiv.ch>
2023-02-03 13:39:34 +0100

    set up env variable handling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+const env = import.meta.env.VITE_TEST
 </script>
 
 <template>
   <main>
     <h1>Home</h1>
+    <p>Test Variable .env: {{ env }}</p>
   </main>
 </template>
