179038274e1c522547bcb1f18e1e24a49791e0cc
Lea Moser <lea@kampakollektiv.ch>
2023-02-05 21:13:06 +0100

    bugfix

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,19 +1,19 @@
 <template>
   <view-title
     v-if="userdata"
-    :title="`${userdata.name}'s<br>Profile`"
+    :title="`${userdata.name}'s<br>Profil`"
     :icon="ProfileIconUrl"
   />
   <button @click="signOut">logout</button>
 </template>
 
 <script setup lang="ts">
-import { useAuth } from "@/composables/useAuth";
-const { signOut } = useAuth();
-const { userdata } = useUser();
 import ProfileIconUrl from "@/assets/icons/user.svg";
 import ViewTitle from "@/components/layout/ViewTitle.vue";
 import { useUser } from "@/composables/useUser";
+import { useAuth } from "@/composables/useAuth";
+const { signOut } = useAuth();
+const { userdata } = useUser();
 </script>
 
 <style lang="scss" scoped></style>
