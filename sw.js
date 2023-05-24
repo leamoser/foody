88a7c28c228b54101bb6e80400a094d0f25bef0c
leamoser <lea@kampakollektiv.ch>
2023-05-24 13:38:55 +0200

    implement pwa support

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE = "pwabuilder-offline";
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+self.addEventListener("message", (event) => {
+    if (event.data && event.data.type === "SKIP_WAITING") {
+        self.skipWaiting();
+    }
+});
